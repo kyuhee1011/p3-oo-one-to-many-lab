ae0421f0a2c0497da0653aee38cf38ee2b08f18b
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-08-01 23:11:03 -0400

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,35 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name=name
+        self.owner = owner
+    
+        if pet_type not in Pet.PET_TYPES:
+            raise TypeError("Please re-enter the Pet Type from the list")
+        Pet.all.append(self)
+        self.pet_type = pet_type
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name=name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+  
+    def add_pet(self,pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instance of Pet Class") 
+        pet.owner =  self
+
+    def get_sorted_pets(self):
+        namelist = []
+        objlist=[]
+        namelist=[pet.name for pet in Pet.all if pet.owner == self]
+        namelist.sort()
+        for name in namelist :
+            for pet in Pet.all:
+                if pet.name == name:
+                   objlist.append(pet)
+        return objlist
